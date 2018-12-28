388c720c80fdc63e785e3d93bd0536656f5f1464
Guerdal <46222722+Guerdal@users.noreply.github.com>
2018-12-28 15:32:38 +0100

    Update action-app_freebox_telecommande.py

--- action-app_freebox_telecommande.py
+++ action-app_freebox_telecommande.py
@@ -271,7 +271,7 @@ class TelecommandeFreebox(object):
         coming_intent = intent_message.intent.intent_name
 
         print '[Recept] intent {}'.format(coming_intent)
-        if coming_intent == 'Tarlak:ChannelFreebox':
+        if coming_intent == 'Guerdal82:ChannelFreebox':
             self.askFreeboxCommand_callback(hermes, intent_message)
         # more callback and if condition goes here...
 
